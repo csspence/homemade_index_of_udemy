600b103a0af0ed796411c64016a65f256f480ae4
Crew Spence <csspence123@gmail.com>
2020-07-21 06:29:06 -0500

    Created function stub for Udemy toy problem.

---
+++ homemade_index_of_udemy.js
@@ -0,0 +1,8 @@
+/*
+Udemy - JavaScript Algorithms and Data Structures Masterclass (Searching Algorithms)
+homemadeIndexOf
+Write a function homemadeIndexOf that accepts an array and a value.  Loop through the array and check if the current array element is equal to the value.  If it is, return the index at which the element is found.  If the value is never found, return -1.
+
+const homemadeIndexOf = (arr, val) => {
+  
+}

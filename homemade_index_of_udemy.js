12bf3d663dc8ffe3d7913eaba6820d77f73afa0a
Crew Spence <csspence123@gmail.com>
2020-07-21 06:31:35 -0500

    Wrote working solution.

--- homemade_index_of_udemy.js
+++ homemade_index_of_udemy.js
@@ -2,7 +2,13 @@
 Udemy - JavaScript Algorithms and Data Structures Masterclass (Searching Algorithms)
 homemadeIndexOf
 Write a function homemadeIndexOf that accepts an array and a value.  Loop through the array and check if the current array element is equal to the value.  If it is, return the index at which the element is found.  If the value is never found, return -1.
+*/
 
 const homemadeIndexOf = (arr, val) => {
-  
+  for(let i = 0; i < arr.length; i++) {
+    if(arr[i] === val) {
+      return i;
+    }
+  }
+  return -1;
 }
